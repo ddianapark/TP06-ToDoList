776c0226c6537614f71a36c027a1c25c02e5e46c
DIANA IRENE PARK <49377760@ort>
2025-08-06 11:12:16 -0300

    Comenzando

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP06_ToDoList.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
